93fd69ffa678ee111aa14ca3b12b242e9d1d1e2c
Jorge Moreno Morillo <jormorno@gmail.com>
2026-01-29 19:29:53 +0100

    Create kubernetes.py

---
+++ kubernetes.py
@@ -0,0 +1,19 @@
+import subprocess
+
+def run(cmd):
+    print(f"▶ {cmd}")
+    subprocess.run(cmd, shell=True, check=True)
+
+def main():
+    run("kubectl apply -f platform/kube/namespace.yaml")
+    run("kubectl apply -f platform/kube/ratings.yaml")
+    run("kubectl apply -f platform/kube/reviews-svc.yaml")
+    run("kubectl apply -f platform/kube/reviews-v1-deployment.yaml")
+    run("kubectl apply -f platform/kube/reviews-v2-deployment.yaml")
+    run("kubectl apply -f platform/kube/reviews-v3-deployment.yaml")
+    run("kubectl apply -f platform/kube/details.yaml")
+    run("kubectl apply -f platform/kube/productpage.yaml")
+    run("kubectl get all -n cdps-27")
+
+if __name__ == "__main__":
+    main()

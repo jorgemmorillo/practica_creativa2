fee1c0e8f597e7cb481a6807bb96332857636dc9
Jorge Moreno Morillo <jormorno@gmail.com>
2026-01-30 22:43:17 +0100

    Update crearImagenes.py

--- crearImagenes.py
+++ crearImagenes.py
@@ -7,9 +7,7 @@ TAG = "g27"
 IMAGES = [
     ("./bookinfo/src/details", f"{DOCKERHUB_USER}/details:{TAG}"),
     ("./bookinfo/src/ratings", f"{DOCKERHUB_USER}/ratings:{TAG}"),
-    ("./bookinfo/src/reviews/reviews-v1", f"{DOCKERHUB_USER}/reviews-v1:{TAG}"),
-    ("./bookinfo/src/reviews/reviews-v2", f"{DOCKERHUB_USER}/reviews-v2:{TAG}"),
-    ("./bookinfo/src/reviews/reviews-v3", f"{DOCKERHUB_USER}/reviews-v3:{TAG}"),
+    ("./bookinfo/src/reviews", f"{DOCKERHUB_USER}/reviews:{TAG}"),
     ("./bookinfo/src/productpage", f"{DOCKERHUB_USER}/productpage:{TAG}"),
 ]
 

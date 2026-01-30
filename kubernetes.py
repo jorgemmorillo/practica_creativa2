9004bed07a80a81cb9e53cbbf8dd1261c48fa46b
Jorge Moreno Morillo <jormorno@gmail.com>
2026-01-30 17:58:32 +0100

    Update kubernetes.py

--- kubernetes.py
+++ kubernetes.py
@@ -5,15 +5,16 @@ def run(cmd):
     subprocess.run(cmd, shell=True, check=True)
 
 def main():
-    run("kubectl apply -f platform/kube/namespace.yaml")
-    run("kubectl apply -f platform/kube/ratings.yaml")
-    run("kubectl apply -f platform/kube/reviews-svc.yaml")
-    run("kubectl apply -f platform/kube/reviews-v1-deployment.yaml")
-    run("kubectl apply -f platform/kube/reviews-v2-deployment.yaml")
-    run("kubectl apply -f platform/kube/reviews-v3-deployment.yaml")
-    run("kubectl apply -f platform/kube/details.yaml")
-    run("kubectl apply -f platform/kube/productpage.yaml")
+    run("kubectl apply -f bookinfo/platform/kube/namespace.yaml")
+    run("kubectl apply -f bookinfo/platform/kube/ratings.yaml")
+    run("kubectl apply -f bookinfo/platform/kube/reviews-svc.yaml")
+    run("kubectl apply -f bookinfo/platform/kube/reviews-v1-deployment.yaml")
+    run("kubectl apply -f bookinfo/platform/kube/reviews-v2-deployment.yaml")
+    run("kubectl apply -f bookinfo/platform/kube/reviews-v3-deployment.yaml")
+    run("kubectl apply -f bookinfo/platform/kube/details.yaml")
+    run("kubectl apply -f bookinfo/platform/kube/productpage.yaml")
     run("kubectl get all -n cdps-27")
 
 if __name__ == "__main__":
     main()
+
